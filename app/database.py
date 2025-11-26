4211631dbe79cf0c34ad51321fa2a6ba3bc82795
Bruno Peretti <90000878+Brunomperetti@users.noreply.github.com>
2025-11-25 21:39:04 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,31 +1,15 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from dotenv import load_dotenv
 
-load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:tu_password@localhost/catalogo_db"
-
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
 )
 
-engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-# ⭐ ESTA ES LA LÍNEA QUE FALTABA ⭐
 Base = declarative_base()
 
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
