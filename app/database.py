a22f22fa729eb39bc92340636234e6f21863c267
Bruno Peretti <90000878+Brunomperetti@users.noreply.github.com>
2025-11-25 21:48:02 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,15 +1,17 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = "postgresql://catalogo_db_3kjj_user:epW1OkLFqrmWclI21RjeRmmI1BenzWih@dpg-d4j4e1ur433s7394ght0-a.oregon-postgres.render.com/catalogo_db_3kjj"
 
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True,
-)
-
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
