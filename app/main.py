283d12820fcd6178682e401ffe8e9cf20e990aba
Bruno Peretti <90000878+Brunomperetti@users.noreply.github.com>
2026-01-17 10:59:09 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -249,17 +249,15 @@ def catalogo(slug: str, request: Request, q: str = "", categoria: str = "", db: 
     productos = query_db.all()
 
     # armar imagen_url para el template (evita error 500)
-for p in productos:
-    if p.imagen:
-        p.imagen_url = f"/static/empresas/{empresa.slug}/productos/{p.imagen}"
-    else:
-        p.imagen_url = "/static/img/no-image.png"
-
+    for p in productos:
+        if p.imagen:
+            p.imagen_url = f"/static/empresas/{empresa.slug}/productos/{p.imagen}"
+        else:
+            p.imagen_url = "/static/img/no-image.png"
 
     categorias = sorted(
         list({p.categoria for p in productos if p.categoria})
     )
-
 
     return templates.TemplateResponse(
         "catalogo.html",
